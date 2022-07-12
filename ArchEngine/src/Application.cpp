e8f45b2eadfc284cdcbcdb16b0f2c4e15969940f
Kye <kyenelson3@gmail.com>
2022-07-12 11:11:51 -0700

    Initial project setup. GLFW and OpenGL setup

---
+++ ArchEngine/src/Application.cpp
@@ -0,0 +1,40 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const int SCR_WIDTH = 1920;
+const int SCR_HEIGHT = 1080;
+
+int main()
+{
+	glfwInit();
+	const char* glsl_version = "#version 460";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "untitled", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+	}
+
+	glfwTerminate();
+	return 0;
+}
